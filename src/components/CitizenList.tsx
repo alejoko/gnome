f2fcba05c4d91b95c83eaf3921546c5bd7d41903
Alejandro Perez <Alejandro.Perez@ext.euipo.europa.eu>
2020-07-19 01:53:44 +0200

    app with react virtualized

--- src/components/CitizenList.tsx
+++ src/components/CitizenList.tsx
@@ -1,25 +1,73 @@
-import React, { useEffect, FunctionComponent, Fragment } from "react";
+import React, { useEffect, FunctionComponent } from "react";
 import { BrastlewarkCitizen, fetchAllCitizens, getCitizens } from "../store/ducks/";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { Searcher } from "./Searcher";
+import { List, ListRowRenderer, AutoSizer, CellMeasurer, CellMeasurerCache } from "react-virtualized";
 
-export const Citizen: FunctionComponent<{ citizen: BrastlewarkCitizen }> = ({ citizen }) => {
+export const Citizen: FunctionComponent<{ index: number }> = ({ index }) => {
+  const citizens = useSelector(getCitizens);
+  const citizen = citizens[index];
+
   return (
-      <div className="card" key={citizen.id}>
+      <div className="card" key={citizen.id} >
         <div className="title">
         <Link to={`${citizen.id}`}>
           <img alt={citizen.name} src={citizen.thumbnail} />
         </Link>
-          <h2>
-            <Link to={`${citizen.id}`}>
-              {citizen.name}
-            </Link>
-          </h2>
+        <h2>
+          <Link to={`${citizen.id}`}>
+            {citizen.name}
+          </Link>
+        </h2>
         </div>
       </div>
   );
 };
+
+const cache = new CellMeasurerCache({
+  fixedWidth: false,
+  defaultHeight: 100
+});
+
+export const RowRenderer: ListRowRenderer = ({ index, parent, key, style}: any) => {
+  return (
+    <CellMeasurer 
+      key={key}
+      cache={cache}
+      parent={parent}
+      columnIndex={0}
+      rowIndex={index}
+      style={style}
+    >
+      <div style={style}>
+        <Citizen index={ index } />
+      </div>
+    </CellMeasurer>
+  );
+};
+
+class AutosizerWrapper extends React.Component<{ citizens: BrastlewarkCitizen[] }> {
+
+  render() {
+    return (
+      <AutoSizer>
+      {
+        ({ width, height }: any) => {
+          return <List
+            rowCount={this.props.citizens.length}
+            width={width}
+            height={height}
+            deferredMeasurementCache={cache}
+            rowHeight={cache.rowHeight}
+            rowRenderer={RowRenderer}
+          />
+        }
+      }
+      </AutoSizer>
+    )
+  }
+}
 
 export const CitizenList: FunctionComponent = () => {
   const citizens = useSelector(getCitizens)
@@ -29,14 +77,12 @@ export const CitizenList: FunctionComponent = () => {
     dispatch(fetchAllCitizens());
   }, []);
 
+  console.log(citizens.length)
+
   return (
-    <Fragment>
+    <>
       <Searcher />
-      <div className="container">
-        <div> 
-          { citizens.map((citizen: BrastlewarkCitizen) => <Citizen key={citizen.id} {... { citizen }} />) } 
-        </div>
-      </div>
-    </Fragment>
+      <AutosizerWrapper citizens={citizens} />
+    </>
     )
 };
